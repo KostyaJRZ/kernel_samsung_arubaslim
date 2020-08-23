e1492825765e34022584e09f254b5466b16372fd
KostyaJRZ <jrzroyale@gmail.com>
2020-08-23 22:56:54 +0530

    Kernel: Add building scripts

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Main environtment
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/out/arch/arm/boot/zImage
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+CONFIG=arubaslim_defconfig
+CROSS_COMPILE="${PWD}/arm-eabi-4.4.3/bin/arm-eabi-"
+
+# Export
+export ARCH=arm
+export CROSS_COMPILE
+
+# Build start
+mkdir $KERNEL_DIR/out
+make O=out $CONFIG
+make -j$(nproc --all) O=out \
+                      ARCH=arm \
+
+if ! [ -a $KERN_IMG ]; then
+    echo "Build error!"
+    exit 1
+fi
+
+cd $ZIP_DIR
+make clean &>/dev/null
+cd ..
+
+cd $ZIP_DIR
+cp $KERN_IMG zImage
+make normal &>/dev/null
+echo "Flashable zip generated under $ZIP_DIR."
+cd ..
+# Build end
---
+++ gettools.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install build package for debian based linux
+sudo apt install bc bash git-core gnupg build-essential \
+    zip curl make automake autogen autoconf autotools-dev libtool shtool python \
+    m4 gcc libtool zlib1g-dev flex bison libssl-dev
+
+# Clone toolchain
+git clone https://github.com/KostyaJRZ/arm-eabi-4.4.3.git
+
+# Clone AnyKernel3
+git clone https://github.com/KostyaJRZ/AnyKernel3.git -b arubaslim_cm-13.0
